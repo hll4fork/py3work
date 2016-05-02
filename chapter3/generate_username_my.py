353e2b18c87f38c226da36856289d33ec9184922
huanglilong <huanglilongwk@outlook.com>
2016-05-02 16:27:12 +0800

    unique username step 2

--- chapter3/generate_username_my.py
+++ chapter3/generate_username_my.py
@@ -27,22 +27,30 @@ def main():
 
     # process file got from command-line
     users = {}
+    usernames = set()
     for filename in sys.argv[1:]:
         for line in open(filename, encoding="utf8"):
             line = line.rstrip() # remove right and left spacewhites
             if line:
-                user = process_line(line)    # get unique username
+                user = process_line(line, usernames)    # get unique username
                 users[user.surname.lower(), user.forename.lower(), user.id] = user
                 print(user.username)
 
-def process_line(line):
+def process_line(line, usernames):
     fields = line.split(":")
-    username = generate_name(fields)
+    username = generate_name(fields, usernames)
     user = User(username, fields[FORENAME], fields[MIDNAME], fields[SURNAME], fields[ID])
     return user
 
-def generate_name(fields):
+def generate_name(fields, usernames):
     username = fields[FORENAME][0] + fields[MIDNAME][:1] + fields[SURNAME]
+    count = 1
+    origin_name = username
+    while username in usernames:
+        username = origin_name
+        username = "{0}{1}".format(username, count)
+        count += 1
+    usernames.add(username)
     return username
 
 main()
